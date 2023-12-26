e33582ce423779e8a255d8e8e95ed91dea180e09
vuxbasnin <banincora@gmail.com>
2023-12-26 23:15:54 +0700

    [update] clean

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,11 +8,11 @@ module.exports = {
     networks: {
         bsctest: {
             url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-            accounts: [process.env.PRIV_KEY],
+            accounts: process.env.PRIV_KEY,
         },
         ethtest: {
             url: `https://sepolia.infura.io/v3/${process.env.ALCHEMY_API_KEY}`,
-            accounts: [process.env.GOERLI_PRIVATE_KEY],
+            accounts: process.env.GOERLI_PRIVATE_KEY,
         },
     },
     etherscan: {
