4524e55820fa5b50370ca8ecab046181ea4c3afa
Nin <ninvuba@vccorp.vn>
2023-12-26 17:15:47 +0700

    [update] config

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -6,6 +6,41 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
 contract Vault is Ownable, AccessControlEnumerable {
-    IERC20 private token;
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
     
+    IERC20 private token;   //set token can deposit or withdraw
+    uint256 public maxWithdrawAmount;
+    bool public withdrawEnabled;
+    bytes32 public constant WITHDRAWER_ROLE = keccak256("WITHDRAWER_ROLE");  //access role
+
+    modifier onlyWithdrawer(){
+        require(owner() == _msgSender() || hasRole(WITHDRAWER_ROLE, _msgSender()), "Caller is not a withdrawer");
+        _;
+    }
+
+    function setWithdrawEnable(bool _isEnable) public onlyOwner {
+        withdrawEnabled = _isEnable;
+    }
+
+    function setMaxWithdrawAmount(uint256 _maxAmount) public onlyOwner {
+        maxWithdrawAmount = _maxAmount;
+    }
+
+    function setToken(IERC20 _token) public onlyOwner {
+        token = _token;
+    }
+
+    function withdraw(address _to, uint256 _amount) external onlyWithdrawer(){
+            require(withdrawEnabled, "Withdraw is not available");
+            require(_amount <= maxWithdrawAmount, "Exceed maximum amount");
+            token.transfer(_to, _amount);
+    }
+
+    function deposit(uint256 _amount) external {
+        require(token.balanceOf(msg.sender) => _amount, "Insufficient account balance);
+        SafeERC20.safeTransferFrom(token, msg.sender, address(this), _amount);
+    }
 }
---
+++ scripts/config.ts
@@ -0,0 +1,38 @@
+//this file update file config.json
+import { promises as fs } from 'fs';
+
+var config: any;
+
+export async function initConfig() {
+    console.log('init');
+    config = JSON.parse((await fs.readFile('../config.json')).toString());
+    return config;
+}
+
+export function getConfig() {
+    return config;
+}
+
+export function setConfig(_path: string, _val: string) {
+    console.log(config);
+    const splitPath = _path.split('.').reverse();
+
+    var ref = config;
+    while (splitPath.length > 1) {
+        let key = splitPath.pop();
+        if (key) {
+            if (!ref[key]) ref[key] = {};
+            ref = ref[key];
+        } else {
+            return;
+        }
+    }
+
+    let key = splitPath.pop();
+    if (key) ref[key] = _val;
+}
+
+export async function updateConfig() {
+    console.log('write: ', JSON.stringify(config));
+    return fs.writeFile('../config.json', JSON.stringify(config, null, 2));
+}
