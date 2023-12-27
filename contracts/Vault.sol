f2b9750d36eb211333fee4651e6444c1e0a7b410
Nin <ninvuba@vccorp.vn>
2023-12-27 10:42:17 +0700

    [update] clean code

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -3,21 +3,24 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
 
 contract Vault is Ownable, AccessControlEnumerable {
-
-    constructor() {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-    }
     
-    IERC20 private token;   //set token can deposit or withdraw
+    constructor() Ownable(_msgSender()) {
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    IERC20 private token; //set token can deposit or withdraw
     uint256 public maxWithdrawAmount;
     bool public withdrawEnabled;
-    bytes32 public constant WITHDRAWER_ROLE = keccak256("WITHDRAWER_ROLE");  //access role
+    bytes32 public constant WITHDRAWER_ROLE = keccak256("WITHDRAWER_ROLE"); //access role
 
-    modifier onlyWithdrawer(){
-        require(owner() == _msgSender() || hasRole(WITHDRAWER_ROLE, _msgSender()), "Caller is not a withdrawer");
+    modifier onlyWithdrawer() {
+        require(
+            owner() == _msgSender() || hasRole(WITHDRAWER_ROLE, _msgSender()),
+            "Caller is not a withdrawer"
+        );
         _;
     }
 
@@ -33,14 +36,17 @@ contract Vault is Ownable, AccessControlEnumerable {
         token = _token;
     }
 
-    function withdraw(address _to, uint256 _amount) external onlyWithdrawer(){
-            require(withdrawEnabled, "Withdraw is not available");
-            require(_amount <= maxWithdrawAmount, "Exceed maximum amount");
-            token.transfer(_to, _amount);
+    function withdraw(address _to, uint256 _amount) external onlyWithdrawer {
+        require(withdrawEnabled, "Withdraw is not available");
+        require(_amount <= maxWithdrawAmount, "Exceed maximum amount");
+        token.transfer(_to, _amount);
     }
 
     function deposit(uint256 _amount) external {
-        require(token.balanceOf(msg.sender) => _amount, "Insufficient account balance);
+        require(
+            token.balanceOf(msg.sender) >= _amount,
+            "Insufficient account balance"
+        );
         SafeERC20.safeTransferFrom(token, msg.sender, address(this), _amount);
     }
 }
